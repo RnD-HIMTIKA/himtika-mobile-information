3eb6e23b1e25cb30f70c6c582d7070996e41b3f0
NaufalSF-Max <naufalsafiqq@gmail.com>
2025-07-14 23:23:17 +0700

    feat: new interface with mapper

--- lib/features/roles/domain/repositories/roles_repository.dart
+++ lib/features/roles/domain/repositories/roles_repository.dart
@@ -1,7 +1,11 @@
+import '../entities/role.dart';
 import '../entities/permission.dart';
+import '../entities/user_role.dart';
 
 abstract class RolesRepository {
-  Future<List<Permission>> getUserPermissions(String userId);
+  Future<List<Role>> getAllRoles(String userId);
   Future<void> assignRole(String userId, String roleId);
   Future<void> revokeRole(String userId, String roleId);
+  Future<List<Permission>> getUserPermissions(String userId);
+  Future<List<UserRole>> getUserRoles(String userId);
 }
