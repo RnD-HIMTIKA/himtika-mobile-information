d07e701e063f08f08cb3aed9ad71da54cb44aa31
NaufalSF-Max <naufalsafiqq@gmail.com>
2025-07-25 10:43:20 +0700

    feat: extend RolesRepository with role-permission methods

--- lib/features/roles/domain/repositories/roles_repository.dart
+++ lib/features/roles/domain/repositories/roles_repository.dart
@@ -1,6 +1,7 @@
 import '../entities/role.dart';
 import '../entities/permission.dart';
 import '../entities/user_role.dart';
+import '../entities/role_permission.dart';
 
 abstract class RolesRepository {
   Future<List<Role>> getAllRoles(String userId);
@@ -8,4 +9,7 @@ abstract class RolesRepository {
   Future<void> revokeRole(String userId, String roleId);
   Future<List<Permission>> getUserPermissions(String userId);
   Future<List<UserRole>> getUserRoles(String userId);
+  Future<void> assignPermissionToRole(String roleId, String permissionId);
+  Future<void> revokePermissionFromRole(String roleId, String permissionId);
+  Future<List<RolePermission>> getPermissionsByRoleId(String roleId);
 }
