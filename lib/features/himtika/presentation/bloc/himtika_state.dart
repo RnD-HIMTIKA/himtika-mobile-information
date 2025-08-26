3d01947cbec15f913da7f6a9a6ffaec93ca6a78a
Ferdi Yansah <frdynsh11@gmail.com>
2025-08-26 19:10:47 +0700

    membuat page utama dari informasi hima

---
+++ lib/features/himtika/presentation/bloc/himtika_state.dart
@@ -0,0 +1,27 @@
+part of 'himtika_bloc.dart';
+
+enum HimtikaStatus { initial, loading, success, failure }
+
+class HimtikaState extends Equatable {
+  const HimtikaState({
+    this.status = HimtikaStatus.initial,
+    this.importantParts = const [],
+  });
+
+  final HimtikaStatus status;
+  // Untuk daftar "Bagian Penting dalam HIMTIKA"
+  final List<Map<String, String>> importantParts;
+
+  HimtikaState copyWith({
+    HimtikaStatus? status,
+    List<Map<String, String>>? importantParts,
+  }) {
+    return HimtikaState(
+      status: status ?? this.status,
+      importantParts: importantParts ?? this.importantParts,
+    );
+  }
+
+  @override
+  List<Object> get props => [status, importantParts];
+}
