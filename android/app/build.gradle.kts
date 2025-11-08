7076bd79459af3e34f82a243f741161d52645b5e
Ferdi Yansah <frdynsh11@gmail.com>
2025-11-08 10:24:44 +0700

    fix: fixed login callback

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.himtika_mobile_information"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 23
+        minSdkVersion (flutter.minSdkVersion)
         compileSdk = flutter.compileSdkVersion
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
